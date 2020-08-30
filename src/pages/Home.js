636a17ccb5db9e686ac2603e529171cba4f3e782
AbuBackker <abubackker11@gmail.com>
2020-08-30 16:43:10 +0600

    Lifting state up added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 // import IntroToFroms from "../components/IntroToFroms/IntroToFroms";
-import ConditionalRendering from "../components/ConditionalRendering/ConditionalRendering";
+// import ConditionalRendering from "../components/ConditionalRendering/ConditionalRendering";
+import LiftingStateUp from "../components/LiftingStateUp/LiftingStateUp";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         {/* <IntroToFroms /> */}
-        <ConditionalRendering />
+        {/* <ConditionalRendering /> */}
+        <LiftingStateUp />
       </div>
     );
   }
